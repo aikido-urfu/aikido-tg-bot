43409e92738ffc5663f67380f0062f4a0cc630e0
MintBrain <maximka602@gmail.com>
2023-11-20 16:15:07 +0500

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import json
+import pickle
+import telegram
+import telethon
+from telethon import TelegramClient
+
+
+bot_token = ''
+chat_id = -1
+
+
+def load_settings():
+    global bot_token, chat_id
+    with open('./settings.json', 'r', encoding='utf-8') as f:
+        settings = json.loads(f.read())
+        bot_token = settings['bot_token']
+        chat_id = settings['chat_id']
+
+
+async def main():
+    bot = telegram.Bot(bot_token)
+
+    async with bot:
+        print(await bot.get_me())
+        last_msgs = (await bot.get_updates())
+        print(last_msgs)
+        await bot.send_message(text=f'Hello ', chat_id=chat_id)
+
+if __name__ == '__main__':
+    load_settings()
+    asyncio.run(main())
