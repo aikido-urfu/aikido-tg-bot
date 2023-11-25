a878e3a0388384d98872a2bd2d8165221f548581
MintBrain <maximka602@gmail.com>
2023-11-25 16:11:21 +0500

    Added commands

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command, CommandObject, CommandStart
+from aiogram.enums import ParseMode
 import asyncio
+import logging
 import json
 
 
 bot_token = ''
 chat_id = -1
+bot: Bot
+dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+
 
 def load_settings():
     global bot_token, chat_id
@@ -16,17 +21,44 @@ def load_settings():
         chat_id = settings['chat_id']
 
 
-# @client.on(events)
+# /start handler
+@dp.message(CommandStart(deep_link=True))  # , magic=F.args.regexp(re.compile(r'Regex   token'))
+async def start(message: types.Message, command: CommandObject):
+    command_args: str = command.args
+    await message.answer(f'StartToken = {command_args}')
+    await message.answer('Вы подписаны на уведомления')
+
+
+# /disable handler
+@dp.message(Command('disable'))
+async def disable(message: types.Message, command: CommandObject):
+    await message.answer('Уведомления отключены')
+
+
+# /mail handler
+@dp.message(Command('mail'))
+async def mail(message: types.Message, command: CommandObject):
+    sender_name: str = 'Max'
+    link: str = 'https://aikido.ru/mail/'
+    await message.answer(f'Вам пришло письмо от *{sender_name}*\n\n'
+                         f'[Перейти на сайт]({link})')
+
+
+# /votes handler
+@dp.message(Command('votes'))
+async def votes(message: types.Message, command: CommandObject):
+    vote_name: str = 'Выбор названия клуба'
+    start, end = '11\.11\.2011', '12\.12\.2022'
+    link: str = 'https://aikido.ru/votes/1/'
+    await message.answer(f'Вы участвуете в голосовании: *{vote_name}*\n'
+                         f'Сроки голосования: {start} \- {end}\n\n'
+                         f'[Перейти к голосованию]({link})')
 
 
 async def main():
-    bot = telegram.Bot(bot_token)
-
-    async with bot:
-        print(await bot.get_me())
-        last_msgs = (await bot.get_updates())
-        print(last_msgs)
-        await bot.send_message(text=f'Hello ', chat_id=chat_id)
+    global bot, dp
+    bot = Bot(bot_token, parse_mode=ParseMode.MARKDOWN_V2)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     load_settings()
