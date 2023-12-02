eb4c76da37a4d3c8056b943b73244337c95124ac
MintBrain <maximka602@gmail.com>
2023-12-02 17:50:45 +0500

    requests get/post api

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from aiogram.enums import ParseMode
 import asyncio
 import logging
 import json
+import requests
 
 
 bot_token = ''
 bot: Bot
 dp = Dispatcher()
 logging.basicConfig(level=logging.INFO)
+url = 'http://aikido.ru/tg/'
 
 
 def load_settings():
@@ -23,34 +25,51 @@ def load_settings():
 @dp.message(CommandStart(deep_link=True))  # , magic=F.args.regexp(re.compile(r'Regex   token'))
 async def start(message: types.Message, command: CommandObject):
     command_args: str = command.args
-    await message.answer(f'StartToken \= {command_args}')
-    await message.answer('Вы подписаны на уведомления')
+    res = requests.post(url + 'start', data={'tgID': message.chat.id, 'token': command_args})
+    if res.ok:
+        await message.answer('Вы подписаны на уведомления')
+    else:
+        await message.answer('Произошла ошибка при подписке на уведомления')
 
 
-# /disable handler
-@dp.message(Command('disable'))
-async def disable(message: types.Message, command: CommandObject):
-    await message.answer('Уведомления отключены')
+# /unsubscribe handler
+@dp.message(Command('unsubscribe'))
+async def unsubscribe(message: types.Message):
+    res = requests.post(url + 'unsubscribe', data={'tgID': message.chat.id})
+    if res.ok:
+        await message.answer('Вы отписаны от уведомлений')
+    else:
+        await message.answer('Ошибка при отписке')
 
 
 # /mail handler
 @dp.message(Command('mail'))
 async def mail(message: types.Message, command: CommandObject):
-    sender_name: str = 'Max'
-    link: str = 'https://aikido.ru/mail/'
-    await message.answer(f'Вам пришло письмо от *{sender_name}*\n\n'
-                         f'[Перейти на сайт]({link})')
+    res = requests.get(url + 'mail', data={'tgID': message.chat.id})
+    if res.ok:
+        ans = res.json()
+        sender_name: str = ans['sender_name']
+        link: str = 'https://aikido.ru/mail/'
+        await message.answer(f'Вам пришло письмо от *{sender_name}*\n\n'
+                             f'[Перейти на сайт]({link})')
+    else:
+        await message.answer('Ошибка при проверке почты')
 
 
 # /votes handler
 @dp.message(Command('votes'))
 async def votes(message: types.Message, command: CommandObject):
-    vote_name: str = 'Выбор названия клуба'
-    start, end = '11\.11\.2011', '12\.12\.2022'
-    link: str = 'https://aikido.ru/votes/1/'
-    await message.answer(f'Вы участвуете в голосовании: *{vote_name}*\n'
-                         f'Сроки голосования: {start} \- {end}\n\n'
-                         f'[Перейти к голосованию]({link})')
+    res = requests.get(url + 'mail', data={'tgID': message.chat.id})
+    if res.ok:
+        ans = res.json()
+        vote_name: str = ans['vote_name']
+        start, end = ['start'], ['end']
+        link: str = f'https://aikido.ru/votes/{ans["vote_id"]}'
+        await message.answer(f'Вы участвуете в голосовании: *{vote_name}*\n'
+                             f'Сроки голосования: {start} \- {end}\n\n'
+                             f'[Перейти к голосованию]({link})')
+    else:
+        await message.answer('Ошибка при проверке голосований')
 
 
 async def main():
